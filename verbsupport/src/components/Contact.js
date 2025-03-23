9649e6fbdf2a5ef66757c75ad5cb35c80a6cadfa
Eden Dubrovsky <ed239@njit.edu>
2025-03-23 13:31:26 -0400

    updating contact logos

--- verbsupport/src/components/Contact.js
+++ verbsupport/src/components/Contact.js
@@ -9,8 +9,10 @@ const Contact = () => {
         <div className='column'>
             <div className='textC'>
                 <h1>Contact Information</h1>
-                <img src={logo} className='App-logoC' alt='Logo' style={{ marginRight: '5rem' }}></img>
-                <img src={logoRu} className='App-logoC' alt='Logo'></img>
+                <div className="logo-container">
+                        <img src={logo} className='App-logoC' alt='Logo' />
+                        <img src={logoRu} className='App-logoC' alt='Logo' />
+                </div>
                 <h2>For any issues or inquiries please email: <br></br><br></br> <a href="mailto:onthegolanguages@gmail.com" style={{ color: 'white' }}>onthegolanguages@gmail.com</a></h2>
             
             </div> 
